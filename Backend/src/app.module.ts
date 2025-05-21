b34a5d63de838129e84e0342f7e1e46a1821a666
cl0ver012 <goodcristina2023@gmail.com>
2025-05-21 17:23:50 +0000

    updated mongodb connection

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,25 +1,31 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatsModule } from './chats/chats.module';
 import { GraphDataModule } from './graph-data/graphData.module';
-import { ConfigModule } from '@nestjs/config';
-
-ConfigModule.forRoot();
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://emmanuelakwuba57:cfaxhgu5GSVBkAtu@emirace.zci1zhz.mongodb.net/graphDB?retryWrites=true&w=majority&appName=Emirace',
-      {
-        connectionName: 'graphDB', // Named connection for graph data
-      },
-    ),
-    GraphDataModule,
+    MongooseModule.forRoot(process.env.MONGODB_CHATS_URI || 'mongodb://localhost:27017/chats', {
+      connectionName: 'chatsDB',
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_GRAPH_URI || 'mongodb://localhost:27017/Graph-Index', {
+      connectionName: 'graphDB',
+    }),
     ChatsModule,
-  ],
+    GraphDataModule
+  ],  
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('MONGODB URIs:', {
+      chats: process.env.MONGODB_CHATS_URI,
+      graph: process.env.MONGODB_GRAPH_URI
+    });
+  }
+}
